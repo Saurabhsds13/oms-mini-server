a0f13cbe43102c815ff19007e90078657db221dc
Saurabh Sonawane <saurabh.sonawane@ael.in>
2024-11-11 15:52:50 +0530

    First Commit

---
+++ src/test/java/in/dmart/oms/OrderManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package in.dmart.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
